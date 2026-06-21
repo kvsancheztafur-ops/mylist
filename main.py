034027d4003b9702a59e66f94338e7446e872d7d
kvsancheztafur-ops <kvsanchez.tafur@gmail.com>
2026-06-21 11:10:34 -0500

    Create main.py
    Datos sobre mí

---
+++ main.py
@@ -0,0 +1,3 @@
+import random
+emojis = ["me llamo Kytzia", "tengo 11 años", " mi perrito se llama goku", " mi comida favotrita es el aji de gallina", "(me gusta leer)"]
+print(random.choice(emojis))
